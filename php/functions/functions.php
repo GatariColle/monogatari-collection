9dfb484c7099e2987a6cf6799939df46047713ab
haide <funcrafter123456@gmail.com>
2021-04-26 21:59:24 +0600

    Get title INFO and visit counter

--- php/functions/functions.php
+++ php/functions/functions.php
@@ -11,10 +11,27 @@ function gettitlesformainrecent()
 function gettitlesformainpopular()
 {
     require('connection.php');
-    $sql = "SELECT title_id, title_name, title_description, title_cover from titles ORDER BY visit_count DESC LIMIT 8";
+    $sql = "SELECT title_id, title_name, title_description, title_cover from titles ORDER BY visit_counter DESC LIMIT 8";
     $res = $con->query($sql);
     $cards = $res->fetch_all(MYSQLI_ASSOC);
     $con->close();
     return $cards;
+}
+function gettitleinfo()
+{
+    $id = $_REQUEST['title_id'];
+    require('connection.php');
+    $sql = "SELECT * from titles where title_id = ".$id." ";
+    $res = $con->query($sql);
+    $cards = $res->fetch_all(MYSQLI_ASSOC);
+    $con->close();
+    return $cards;
+}
+function visitcounter()
+{
+    $id = $_REQUEST['title_id'];
+    require('connection.php');
+    $sql = "UPDATE titles SET visit_counter = visit_counter + 1 where title_id = ".$id." ";
+    $res = $con->query($sql);
 }
 ?>
